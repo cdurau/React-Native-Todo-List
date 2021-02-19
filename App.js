d36ea35d066d8677357be0c632a0cc9b5acdd520
cdurau <{ID}+{username}@users.noreply.github.com>
2021-02-19 19:06:37 +0100

    + FlatList

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
+    const [todos, setTodos] = useState([
+        { text: 'buy coffee', key: '1' },
+        { text: 'create an app', key: '2' },
+        { text: 'play on the switch', key: '3' }
+    ]);
+
     return (
-        <View style={styles.container}>
-            <Text>Hallo Welt</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            {/* Header */}
+            <View style={styles.content}>
+                {/* Todo Form */}
+                <View style={styles.list}>
+                    <FlatList
+                        // prettier-ignore
+                        data={todos}
+                        renderItem={({ item }) => <Text>{item.text}</Text>}
+                    />
+                </View>
+            </View>
+        </SafeAreaView>
     );
 }
 
@@ -13,5 +30,9 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff'
+    },
+    content: {},
+    list: {
+        color: '#000'
     }
 });
